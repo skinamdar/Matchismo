905741dc0734eb5ac10ea8ab84ca03f07f170df5
Syeda Inamdar <sinamdar@Administrators-MacBook-2.local>
2013-08-20 13:16:08 -0700

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Syeda Inamdar on 8/20/13.
+//  Copyright (c) 2013 Syeda Inamdar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Syeda Inamdar on 8/20/13.
+//  Copyright (c) 2013 Syeda Inamdar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
